2bdb6d063a4c2af529bc1df7dee33e4946d80319
unknown <renedp1975@gmail.com>
2022-10-24 14:19:48 -0500

    form

--- pages/components/Tab03.js
+++ pages/components/Tab03.js
@@ -3,6 +3,7 @@ import Opcion2 from "./Opcion2";
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
 
 const ImagenDerecha1 = () => {
   return (
@@ -22,54 +23,78 @@ const ImagenDerecha2 = () => {
 
 
 const VentanaModal = (props) => {
+
+  const label = { inputProps: { 'aria-label': ' Hiermit akzeptiere ich die geltenden Datenschutzbestimmungen.' } };
+
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+      <div className=" justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-6 mx-auto max-w-3xl w-[40vw]">
           {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <div className="border-0 pt-6 pb-10 rounded-sm shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
-            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-              <h3 className="text-3xl font-semibold">
-                Modal Title
-              </h3>
-              <button
-                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                onClick={() => props.setShowModal(false)}
-              >
-                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                <img src="/img/close.svg" alt="NordWest" className="  w-[100vw] " />
+            <div className="flex items-start justify-between p-1  ">
+
+              <img src="/img/nwm_logo_400px.png" />
+
+              <button className="p-1 ml-auto  border-0 text-black  float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                onClick={() => props.setShowModal(false)}>
+
+                <span className=" opacity-1 mr-4 h-8 w-8 text-2xl block outline-none focus:outline-none">
+                  <img src="/img/ei_close.svg" />
+
+
                 </span>
               </button>
             </div>
             {/*body*/}
-            <div className="relative p-6 flex-auto">
-              <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                I always felt like I could do anything. That’s the main
-                thing people are controlled by! Thoughts- their perception
-                of themselves! They're slowed down by their perception of
-                themselves. If you're taught you can’t do anything, you
-                won’t do anything. I was taught I could do everything.
-              </p>
-              <TextField id="standard-basic" label="Standard" variant="standard" />
+            <div className="relative  mx-10 flex-auto font-roboto">
+              <div className=" font-bold mt-6 text-[0.9rem]">Jetzt bewerben</div>
+              <div className=" mt-1 text-[0.6rem]">Du haben es fast geschafft!</div>
+              <div className=" mt-1 text-[0.9rem]">So können wir Dich erreichen</div>
+              <div>
+                <TextField id="standard-basic" label="Vorname" variant="standard" />
+              </div>
+              <div>
+                <TextField id="nachname" label="Nachname" variant="standard" />
+              </div>
+
+              <div>
+                <TextField id="Postleitzahl" label="Postleitzahl" variant="standard" />
+              </div>
+
+              <div>
+                <TextField id="Email" label="Email" variant="standard" />
+              </div>
+
+              <div>
+                <TextField id="Telefon" label="Telefon" variant="standard" />
+              </div>
+
+            
+
+
+
+
+
+
+              <div className="mt-2">
+                <button
+                  className="btn bg-[#E20613] rounded text-[1rem] text-[#ffffff] px-7 py-1 font-roboto ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={() => props.setShowModal(false)}
+                >
+                  Absenden
+                </button>
+              </div>
+
+              <div>
+                <Checkbox {...label} size="small"  sx={{ '& .MuiSvgIcon-root': { fontSize: 14 } }} />
+
+              </div>
+
             </div>
-            {/*footer*/}
-            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-              <button
-                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => props.setShowModal(false)}
-              >
-                Close
-              </button>
-              <button
-                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => props.setShowModal(false)}
-              >
-                Save Changes
-              </button>
-            </div>
+
           </div>
         </div>
       </div>
